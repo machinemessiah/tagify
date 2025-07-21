6908fe1417d3289046bba8230e81f40037c3bce6
Alexander Kepekci <alexkepekci@hotmail.com>
2025-07-21 04:46:39 -0400

    remove version from extension

--- src/extensions/extension.js
+++ src/extensions/extension.js
@@ -4,7 +4,6 @@
     await new Promise((resolve) => setTimeout(resolve, 100));
   }
   const APP_NAME = "tagify";
-  const VERSION = "1.0.0";
 
   const STORAGE_KEY = "tagify:tagData";
   const PLAYLIST_CACHE_KEY = "tagify:playlistCache";
