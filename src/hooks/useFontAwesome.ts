a3eae680bdfb556cc81aa46084d6198c75fa817e
Alexander Kepekci <alexkepekci@Alexanders-MBP.lan>
2025-09-02 07:35:50 -0400

    update to FontAwesome 7

--- src/hooks/useFontAwesome.ts
+++ src/hooks/useFontAwesome.ts
@@ -6,7 +6,7 @@ export function useFontAwesome() {
       const link = document.createElement("link");
       link.id = "font-awesome-css";
       link.rel = "stylesheet";
-      link.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css";
+      link.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css";
       document.head.appendChild(link);
     }
   }, []);
